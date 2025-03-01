a16b981a8fab885f8cb57fae2a2364efbd1b4fa6
lordtimzki <tdacalos@uci.edu>
2025-03-01 12:37:28 -0800

    Basic design finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import "./App.css";
+import Logo from "./assets/Logo.png";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="info">
+        <img className="logo" src={Logo} />
+        <h1>Guess the NBA Silhouettes!</h1>
+        <h3>
+          Do you have elite ball knowledge? Test your knowledge with these
+          players!
+        </h3>
+        <p>Players: </p>
+      </div>
+    </>
+  );
 }
 
 export default App;
